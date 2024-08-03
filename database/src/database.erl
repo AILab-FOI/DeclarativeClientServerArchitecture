7e6ec074ccc29221f3ea24b44b3d75e56d321625
filip.kranjec <filip.kranjec@invariant.hr>
2024-08-03 20:40:46 +0200

    request implemented

--- database/src/database.erl
+++ database/src/database.erl
@@ -1,14 +1,15 @@
 -module(database).
 
--export([install/1, start/2, stop/1, dodaj_studenta/4, dohvati_studenta/1]).
+-export([install/1, start/2, stop/1, dodaj_studenta/4, dohvati_studenta/1,
+         dohvati_fakultet/1]).
 
 -record(db_fakultet, {uuid, naziv, adresa, lokacija}).
 -record(db_katedra, {uuid, naziv}).
 -record(db_student, {uuid, ime, prezime, oib, lozinka}).
--record(db_djelatnik, {uuid, naziv, oib}).
--record(db_kolegij, {uuid, naziv, lekcija}).
+-record(db_djelatnik, {uuid, naziv, oib, uuid_katedra}).
+-record(db_kolegij, {uuid, naziv, lekcija, uuid_nositelj}).
 -record(db_lekcija, {uuid, naziv, sadrzaj}).
--record(db_kviz, {uuid, naziv}).
+-record(db_kviz, {uuid, naziv, uuid_lekcija}).
 
 start(normal, []) ->
     mnesia:wait_for_tables([db_ustanove, db_korisnik], 5000),
@@ -24,6 +25,22 @@ install(Nodes) ->
                         [{attributes, record_info(fields, db_fakultet)},
                          {index, [#db_fakultet.uuid]},
                          {disc_copies, Nodes}]),
+    mnesia:create_table(db_katedra,
+                        [{attributes, record_info(fields, db_katedra)},
+                         {index, [#db_katedra.uuid]},
+                         {disc_copies, Nodes}]),
+    mnesia:create_table(db_kolegij,
+                        [{attributes, record_info(fields, db_kolegij)},
+                         {index, [#db_kolegij.uuid]},
+                         {disc_copies, Nodes}]),
+    mnesia:create_table(db_lekcija,
+                        [{attributes, record_info(fields, db_lekcija)},
+                         {index, [#db_lekcija.uuid]},
+                         {disc_copies, Nodes}]),
+    mnesia:create_table(db_kviz,
+                        [{attributes, record_info(fields, db_kviz)},
+                         {index, [#db_kviz.uuid]},
+                         {disc_copies, Nodes}]),
     mnesia:create_table(db_student,
                         [{attributes, record_info(fields, db_student)},
                          {index, [#db_student.uuid]},
@@ -31,6 +48,7 @@ install(Nodes) ->
     rpc:multicall(Nodes, application, stop, [mnesia]).
 
 dodaj_studenta(Ime, Prezime, Oib, Lozinka) ->
+    io:format(Ime),
     Fun = fun() ->
              mnesia:write(#db_student{ime = Ime,
                                       prezime = Prezime,
@@ -55,6 +73,18 @@ dohvati_studenta(Uuid) ->
                               oib = O,
                               lozinka = L}] ->
                      {Uuid, I, P, O, L};
+                 [] -> undefined
+             end
+          end,
+    mnesia:transaction(Fun).
+
+dohvati_fakultet(Uuid) ->
+    Fun = fun() ->
+             case mnesia:read({db_fakultet, Uuid}) of
+                 [#db_fakultet{naziv = N,
+                               adresa = A,
+                               lokacija = L}] ->
+                     {Uuid, N, A, L};
                  [] -> undefined
              end
           end,
--- server/src/server_login_handler.erl
+++ server/src/server_login_handler.erl
@@ -1,16 +1,58 @@
 -module(server_login_handler).
 
+-import(database, [dodaj_studenta/4]).
+
 -behaviour(cowboy_handler).
 
--export([init/2]).
+-export([init/2, allowed_methods/2, content_types_accepted/2, charsets_provided/2,
+         from_json/2, content_type_provided/2, to_json/2]).
 
-init(Req0, State) ->
-    {ok, Data, _Req} = cowboy_req:read_body(Req0),
-    Req = cowboy_req:reply(200, #{}, Data, Req0),
-    Obj = jiffy:decode(Data, [return_maps]),
-    Ime = maps:get(<<"ime">>, Obj),
-    Asd = maps:get(<<"asd">>, Obj),
-    io:format(Asd),
-    io:format(Ime),
+init(Req, State) ->
+    {cowboy_rest, Req, State}.
 
-    {ok, Req, State}.
+allowed_methods(Req, State) ->
+    {[<<"PUT">>], Req, State}.
+
+content_types_accepted(Req, State) ->
+    {[{{<<"application">>, <<"json">>, []}, from_json}], Req, State}.
+
+content_type_provided(Req, State) ->
+    {[{{<<"application">>, <<"json">>, []}, to_json}], Req, State}.
+
+charsets_provided(Req, State) ->
+    {[<<"utf-8">>], Req, State}.
+
+from_json(Req, State) ->
+    json_request(Req, State).
+
+to_json(Req, State) ->
+    json_request(Req, State).
+
+json_request(Req, State) ->
+    case gather(Req) of
+        {error, Reason} ->
+            err(400, Reason, Req, State);
+        {ok, Map, Req2} ->
+            run_request(Map, Req2, State)
+    end.
+
+run_request(Map, Req, State) ->
+    Body = jiffy:encode(Map),
+    Req2 = cowboy_req:set_resp_body(Body, Req),
+    database:dodaj_studenta(<<"">>, <<"">>, <<"">>, <<"">>),
+    io:format(<<"ok">>),
+    Reply = cowboy_req:reply(200, Req2),
+    {stop, Reply, State}.
+
+gather(Req) ->
+    {ok, Body, Req2} = cowboy_req:read_body(Req),
+    Map = jiffy:decode(Body, [return_maps]),
+    {ok, Map, Req2}.
+
+err(Code, Reason, Req, State) ->
+    Formatted = iolist_to_binary(io_lib:format("~p", [Reason])),
+    Err = #{type => error, message => Formatted},
+    Body = jiffy:encode(#{errors => [Err]}),
+    Req2 = cowboy_req:set_resp_body(Body, Req),
+    Reply = cowboy_req:reply(Code, Req2),
+    {stop, Reply, State}.
